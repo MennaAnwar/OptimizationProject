7074cc173f96a672927084548728a76000618f9f
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-05-12 14:03:30 +0300

    break when solution fails

--- Rate/Rate_optimization/rate_opt.py
+++ Rate/Rate_optimization/rate_opt.py
@@ -5,25 +5,26 @@ import matplotlib.pyplot as plt
 # Function used in constraint 2
 def c2(M: cp.Variable, v) -> cp.Variable:
     M_res = M
-    M_res = (M_res - cp.diag(cp.diag(M_res))/np.sqrt(3) + cp.diag(v))
+    M_res = (M_res - cp.diag(cp.diag(M_res))) + cp.diag(v)
     return cp.norm(M_res, axis=1)
 
 # Problem constants
 K = 3 # Number of users
 L = 9 # Number of lamps
-g = np.random.rand(L, K) # Describe the channel
+g = np.random.rayleigh(1, (L, K)) # Describe the channel
 p = np.ones(L) * 10 # Max power for each lamp
 
-sigma_max = 10**-3
+sigma_max = 10**-10
 sigma_n = np.random.rand(K)*sigma_max
 
 # Initial guess for theta
 theta = np.ones(K)
-iterations = 10
-opt_rate = np.zeros(iterations)
+iterations = 500
+opt_rate = np.zeros(iterations) - 1
 
 # Iterate over the problem
 for j in range(iterations):
+    print(j)
     # Problem variables
     w = cp.Variable((L, K))
     u = cp.Variable(K)
@@ -37,21 +38,23 @@ for j in range(iterations):
 
 
     constraints = [
-        cp.diag(M) >= u/(2*theta) + (0.5 * v**2 * theta),
+        cp.diag(M) >= u/(2*theta) + (0.5 * cp.multiply(v**2, theta)),
         c2(M, sigma_n) <= v,
         cp.sum(w, axis=1) <= p
     ]
 
     problem = cp.Problem(objective, constraints)
+    try:
+        problem.solve(solver=cp.SCS) #SCS
+        # update value of theta
+        uj = u.value
+        vj = v.value
+        theta = np.sqrt(uj)/vj
 
-    problem.solve()
-    # update value of theta
-    uj = u.value
-    vj = v.value
-    theta = np.sqrt(uj)/vj
-
-    # add this rate to opt_rate
-    opt_rate[j] = problem.value
+        # add this rate to opt_rate
+        opt_rate[j] = problem.value
+    except:
+        break
 # End of loop
 
 # Algorithm output
@@ -61,8 +64,9 @@ print("w = ", w.value)
 print("optimal rate = ", problem.value)
 
 # Plot output
-n = np.arange(1, iterations+1)
-plt.plot(n, opt_rate)
+res = opt_rate[opt_rate != -1]
+n = np.arange(1, len(res)+1)
+plt.plot(n,res)
 plt.xlabel("Iteration number")
 plt.ylabel("Optimal rate")
 plt.show()
