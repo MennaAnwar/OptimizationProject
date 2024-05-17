000d3b55cd4a37c48d0588f875f52b7034108e24
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-05-17 21:03:23 +0300

    power is alive and playing

--- Power/Localization_cvx/power_cvx.py
+++ Power/Localization_cvx/power_cvx.py
@@ -2,31 +2,33 @@
 
 import cvxpy as cp
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 ### Parameters ###
 
 # number of Transmitters (aka LEDs)
-NL = 8
+NL = 4
 
 # a known matrix that is independent of p and depends on VLP system parameters 
-# size = (3 * NL, 3)
-# gamma = np.ones(size) # belongs to R(3NL*3)
-gamma = np.random.rand(3 * NL, 3) # belongs to R(3NL*3)
+gamma = np.loadtxt("gamma_sys")
 
-# max tolerable CRLB level (will be specified later)
-epsilon = 0.5  
+# max tolerable CRLB level (from 0.05 to 0.75)
+epsilon = np.arange(0.05, 0.76, 0.05)
 
-# assumed electrical-to-optical power conversion efficiency
-powConvEff = 0.3
+# array to store min power values at different CRLB levels
+iterations = len(epsilon)
+minPowArray = np.empty(iterations)
+
+# empty array to store allocated LED power values at CRLB = 0.25
+allocatedPow_CRLB = np.empty(NL)
 
 # upper and lower bounds for electrical power (W)
 elecPow_up = 5
-elecPow_low = 1
+elecPow_low = 0.5
 
-# upper and lower bounds for optical power (W)
-optPow_up = elecPow_up * powConvEff
-optPow_low = elecPow_low * powConvEff
+# assumed electrical-to-optical power conversion efficiency
+powConvEff = 0.25
 
 
 ### Optimization Problem Variable ###
@@ -43,24 +45,50 @@ def FisherInformationMatrix(p):
     return ( cp.kron(np.eye(3), p_matrix).T @ gamma )
 
 
-# Define the objective function (minimize total power)
+### Optimization Problem and Solution ###
+
+# objective function is to minimize total power (1.T @ P)
 objective = cp.Minimize(cp.sum(p))
 
-# Define the constraints
-constraints = [
-    cp.trace(cp.inv_pos(FisherInformationMatrix(p))) <= epsilon,
-    p >= 0 
-    # p <= optPow_up,
-    # p >= optPow_low
-]
+# loop to calculate min power for different values of CRLB
+for i in range(iterations):
+    # constraints 
+    constraints = [
+        cp.trace(cp.inv_pos(FisherInformationMatrix(p))) <= epsilon[i],
+        p >= 0, 
+        p <= elecPow_up,
+        p >= elecPow_low
+    ]
 
-# Form and solve the problem
-problem = cp.Problem(objective, constraints)
-problem.solve(solver=cp.SCS)
+    # Form and solve the problem
+    problem = cp.Problem(objective, constraints)
+    problem.solve(solver=cp.SCS) # selected SCS as the solver
 
-# Print the results
+    # store min power value at this CRLB
+    minPowArray[i] = problem.value
+
+    # store allocated power values at epsilon (CRLB) = 0.25
+    if epsilon[i] == 0.25:
+        allocatedPow_CRLB = p.value
+
+# printing results
 if problem.status == 'optimal':
-    print("Optimal power allocation:", p.value)
-    print("Minimum total power:", problem.value)
+    # print allocated electrical power @CRLB = 0.25
+    print("Optimal Electrical Power Allocation:", allocatedPow_CRLB)
+
+    # save power values in a text file to be used for rate optimization
+    np.savetxt("kono_pawa_elec_w", allocatedPow_CRLB)
+
+    # save power values in a text file to be used for rate optimization
+    np.savetxt("kono_pawa_opt_w", allocatedPow_CRLB * powConvEff)
+
+    # save power values in a text file to be used for rate optimization
+    np.savetxt("kono_pawa_opt_dbm", 10 * np.log10(allocatedPow_CRLB * powConvEff * 1000))
+
+    # plotting Min Power vs CRLB
+    plt.plot(epsilon,minPowArray)
+    plt.xlabel("Max CRLB")
+    plt.ylabel("Min Power")
+    plt.show()
 else:
     print("yo, what the duck?!")
